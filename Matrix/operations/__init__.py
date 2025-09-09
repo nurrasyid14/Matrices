6f2d439a2b7925f8c5fbe8c186042bd01fe3d024
nurrasyid14 <nurrasyid186@gmail.com>
2025-09-10 00:49:20 +0700

    feat: pindahkan operasi matematika ke modul terpisah

---
+++ Matrix/operations/__init__.py
@@ -0,0 +1,4 @@
+# matriks/operations/__init__.py
+from .adder import add_matrices
+from .substractor import subtract_matrices
+from .multiplier import multiply_matrices
---
+++ Matrix/utilities.py
@@ -0,0 +1,7 @@
+# matriks/utilities.py
+def print_matrix(matrix):
+    """
+    Mencetak isi dari objek matriks.
+    """
+    for row in matrix.data:
+        print(row)
