5076b3cd7555297fe1de78a8e728cbc6c24d313f
nurrasyid14 <nurrasyid186@gmail.com>
2025-09-10 00:50:16 +0700

    refactor: buat modul untuk fungsi utilitas

---
+++ Matrix/matrix.py
@@ -0,0 +1,14 @@
+# matrix.py
+class Matrix:
+    """
+    Kelas untuk merepresentasikan objek matriks.
+    """
+    def __init__(self, data):
+        if not isinstance(data, list) or not all(isinstance(row, list) for row in data):
+            raise TypeError("Data harus berupa list of lists.")
+        self.data = data
+        self.rows = len(data)
+        self.cols = len(data[0]) if self.rows > 0 else 0
+
+        if not all(len(row) == self.cols for row in data):
+            raise ValueError("Semua baris harus memiliki jumlah kolom yang sama.")
